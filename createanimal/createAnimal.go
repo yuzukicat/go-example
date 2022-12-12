d4c3d4afddc138b05172c58a173ee9b86923001c
yuzuki.cat <yuzuki.cat@kamisu66.com>
2022-12-12 13:36:15 +0900

    create animal assignment

--- createanimal/createAnimal.go
+++ createanimal/createAnimal.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 )
 
 type Animal interface {
@@ -10,52 +11,105 @@ type Animal interface {
 	Speak()
 }
 
-type AnimalAttr struct {
+type AnimalType struct {
 	food       string
 	locomotion string
 	noise      string
 }
 
-func (animalAttr *AnimalAttr) InitAnimal(name string) {
-	animalMap := make(map[string]AnimalAttr)
-	fmt.Scanln(&name)
-	switch name {
+var animalMap = make(map[string]*AnimalType)
+
+func (animalType *AnimalType) InitAnimal(noa string, toa string) {
+	switch toa {
 	case "cow":
-		animalMap["cow"] = AnimalAttr{
+		cow := AnimalType{
 			food:       "grass",
 			locomotion: "walk",
 			noise:      "moo",
 		}
+		animalMap[noa] = &cow
 		fmt.Println("Created it!")
 	case "bird":
-		animalMap["bird"] = AnimalAttr{
+		bird := AnimalType{
 			food:       "worms",
 			locomotion: "fly",
 			noise:      "peep",
 		}
+		animalMap[noa] = &bird
 		fmt.Println("Created it!")
 	case "snake":
-		animalMap["snake"] = AnimalAttr{
+		snake := AnimalType{
 			food:       "mice",
 			locomotion: "slither",
 			noise:      "hsss",
 		}
+		animalMap[noa] = &snake
 		fmt.Println("Created it!")
+	default:
+		fmt.Println("animal do not have the type you want to create")
 	}
 }
 
-func (animalAttr *AnimalAttr) QueryAnimal(name string, action string) {
-
+func (animalType *AnimalType) QueryAnimal(noa string, aoa string) {
+	var animal Animal
+	animalType = animalMap[noa]
+	if animalType != nil {
+		animal = animalType
+		switch aoa {
+		case "eat":
+			animal.Eat()
+		case "move":
+			animal.Move()
+		case "speak":
+			animal.Speak()
+		default:
+			fmt.Println("animal do not have the action you want to query")
+		}
+	} else {
+		fmt.Println("you have not created the animal you want to query")
+	}
 }
 
-func (animalAttr *AnimalAttr) Eat() {
-	fmt.Println(animalAttr.food)
+func (animalType *AnimalType) Eat() {
+	fmt.Println(animalType.food)
 }
 
-func (animalAttr *AnimalAttr) Move() {
-	fmt.Println(animalAttr.locomotion)
+func (animalType *AnimalType) Move() {
+	fmt.Println(animalType.locomotion)
 }
 
-func (animalAttr *AnimalAttr) Speak() {
-	fmt.Println(animalAttr.noise)
+func (animalType *AnimalType) Speak() {
+	fmt.Println(animalType.noise)
+}
+
+func (animalType *AnimalType) UserInputHandler(userInput string) {
+	r := regexp.MustCompile(`(^newanimal|^query)[^\>](.+)[^\>](cow$|bird$|snake$|eat$|move$|speak$)`)
+	res := r.FindAllStringSubmatch(userInput, -1)
+	if res != nil {
+		req := res[0][1]
+		noa := res[0][2]
+		toaoraoa := res[0][3]
+		// fmt.Println(req)
+		// fmt.Println(noa)
+		// fmt.Println(toaoraoa)
+		switch req {
+		case "newanimal":
+			animalType.InitAnimal(noa, toaoraoa)
+		case "query":
+			animalType.QueryAnimal(noa, toaoraoa)
+		default:
+			fmt.Println("the first string should be newanimal or query")
+		}
+	} else {
+		fmt.Println("please check your input")
+	}
+}
+
+func main() {
+	for i := 0; i > -1; i++ {
+		var animalType AnimalType
+		var req string
+		fmt.Scanln(&req)
+		animalType.UserInputHandler(req)
+	}
 }
